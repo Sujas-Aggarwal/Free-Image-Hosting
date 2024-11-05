9ce26e5f8d9733c356a2ccbbb5dd6758a7dd2880
Sujas <ce1221047@iitd.ac.in>
2024-11-05 12:56:22 +0530

    .gitkeep

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from utility import FreeImageHoster
+from src.utility import FreeImageHoster
 import argparse
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Download Image from URL')
     parser.add_argument('-link', '-l', type=str, help='Indirect link to the image')
     parser.add_argument('-image', '-i', type=str, help='Name of the image to be saved')
     args = parser.parse_args()
-    FreeImageHoster.ScrapeImageFromLink(link=args.link,image_name= args.image)
-    # FreeImageHoster.UploadImage("input/IMG20240813225308.jpg")
+    FreeImageHoster.UploadImage(args.image)
